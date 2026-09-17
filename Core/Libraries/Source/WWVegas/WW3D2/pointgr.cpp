a365871759b950e2c0ca1da3ff5711e51ac1b3d5
Jacob Lane Ledbetter <23038070+CryoTheRenegade@users.noreply.github.com>
2026-09-17 03:19:29 -0600

    fix(pointgroup): Prevent null dereference in PointGroupClass::RenderVolumeParticle() when no size array is given (#3280)
    (cherry picked from commit e2f9440073000fbf2c50a02ac10d6ae237299cea)

--- Core/Libraries/Source/WWVegas/WW3D2/pointgr.cpp
+++ Core/Libraries/Source/WWVegas/WW3D2/pointgr.cpp
@@ -1129,6 +1129,8 @@ void PointGroupClass::Update_Arrays(
 
 		case TRIS_SIZE_NOORIENT:
 			{
+				WWASSERT(point_size);
+
 				// Scale vertex offsets and add them to point locations to get vertex locations
 				for (i = 0; i < active_points; i++) {
 					vertex_loc[vert + 0] = point_loc[i] +
@@ -1144,6 +1146,8 @@ void PointGroupClass::Update_Arrays(
 
 		case TRIS_NOSIZE_ORIENT:
 			{
+				WWASSERT(point_orientation);
+
 				// Scale vertex offsets and add them to point locations to get vertex locations
 				for (i = 0; i < active_points; i++) {
 					vertex_loc[vert + 0] = point_loc[i] +
@@ -1159,6 +1163,8 @@ void PointGroupClass::Update_Arrays(
 
 		case TRIS_SIZE_ORIENT:
 			{
+				WWASSERT(point_size && point_orientation);
+
 				// Scale vertex offsets and add them to point locations to get vertex locations
 				for (i = 0; i < active_points; i++) {
 					vertex_loc[vert + 0] = point_loc[i] +
@@ -1194,6 +1200,8 @@ void PointGroupClass::Update_Arrays(
 
 		case QUADS_SIZE_NOORIENT:
 			{
+				WWASSERT(point_size);
+
 				// Scale vertex offsets and add them to point locations to get vertex locations
 				for (i = 0; i < active_points; i++) {
 					vertex_loc[vert + 0] = point_loc[i] +
@@ -1211,6 +1219,8 @@ void PointGroupClass::Update_Arrays(
 
 		case QUADS_NOSIZE_ORIENT:
 			{
+				WWASSERT(point_orientation);
+
 				// Scale vertex offsets and add them to point locations to get vertex locations
 				for (i = 0; i < active_points; i++) {
 					vertex_loc[vert + 0] = point_loc[i] +
@@ -1228,6 +1238,8 @@ void PointGroupClass::Update_Arrays(
 
 		case QUADS_SIZE_ORIENT:
 			{
+				WWASSERT(point_size && point_orientation);
+
 				Matrix4x4 view;
 				Vector4 result;
 				if (!Billboard) {
@@ -1370,6 +1382,8 @@ void PointGroupClass::Update_Arrays(
 		case SCREEN_SIZE_NOORIENT:
 		case SCREEN_SIZE_ORIENT:
 			{
+				WWASSERT(point_size);
+
 				// Offsets need to be scaled to the current screen resolution
 
    			// First find x and y scale factors (sizes in pixels need to be
@@ -1848,7 +1862,8 @@ void PointGroupClass::RenderVolumeParticle(RenderInfoClass &rinfo, unsigned int 
 			// 3 times per particle when we can do it once
 			float recipDepth = 0.1f / (float)depth;
 
-			float shiftInc = ( t *  *current_size * recipDepth );
+			const float pointSize = current_size ? *current_size : DefaultPointSize;
+			float shiftInc = t * pointSize * recipDepth;
 
 			Vector3 volumeLayerShift;
 			Vector3 cameraPosition = rinfo.Camera.Get_Position();
